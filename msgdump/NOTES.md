64368de296093d06c983433ff0c1580c8d608584
John Nagle <nagle@sitetruth.com>
2018-09-11 21:25:56 -0700

    Clean up

--- msgdump/NOTES.md
+++ msgdump/NOTES.md
@@ -17,7 +17,25 @@ llviewerobject.cpp:
     
     At line 1205, fan out on full or terse update message.
     At line 1258, fan out on message length for full update.
-    There are 
-    multiple message formats defined only by length.
+    There are multiple message formats defined only by length.
     The decoding setup is awful.
     At line 1553, fan out on message length for ImprovedTerseObjectUpdate.
+    
+    This format is simpler than it looks.See "fsmsgdump.h" for what it really is.
+    
+    Potential buffer overflow in llviewerobject.cpp at lines 1235 and 1549:
+    
+    	length = mesgsys->getSizeFast(_PREHASH_ObjectData, block_num, _PREHASH_ObjectData);
+		mesgsys->getBinaryDataFast(_PREHASH_ObjectData, _PREHASH_ObjectData, data, length, block_num);
+		
+    "data" is a 76-byte array on the stack.  "length" comes from the incoming packet. 
+    getBinaryDataFast does not check bounds. An oversized value from the net will clobber the stack.
+    
+    TerseObjectUpdate is described here: http://wiki.secondlife.com/wiki/ImprovedTerseObjectUpdate
+    The section marked "data" there is what we have in fsmsgdump.h. That's documented in
+    for a full update at http://wiki.secondlife.com/wiki/ObjectUpdate#ObjectData_Format
+    Full update and terse update have the same positional data.
+
+
+ 
+    
--- msgdump/fsmsgdump.h
+++ msgdump/fsmsgdump.h
@@ -14,14 +14,16 @@ typedef uint16_t U16Net;                //
 typedef uint32_t LLVector3Net[3];       // an LLVector3 in net order
 typedef uint32_t LLVector4Net[4];
 //
-//  ImprovedTerseObjectUpdate
+//  ObjectUpdateData
 //
+//  This "data" element appears
+//  inside both ObjectUpdateData and ObjectUpdate.
 //  These messages come in a number of fixed lengths, and
-//  are identified only by their length. 
+//  are identified only by their length.  
 //
 //  The 16-byte update
 //
-struct ImprovedTerseObjectUpdate16 {
+struct ObjectUpdateData16 {
     uint8_t position[3];                 // integer-scaled low-accuracy values
     uint8_t velocity[3];
     uint8_t acceleration[3];
@@ -32,7 +34,7 @@ struct ImprovedTerseObjectUpdate16 {
 //
 //  The 32-byte update. Same info, larger numeric values.
 //
-struct ImprovedTerseObjectUpdate32 {
+struct ObjectUpdateData32 {
     U16Net position[3];                 // integer-scaled low-accuracy values
     U16Net velocity[3];
     U16Net acceleration[3];
@@ -43,15 +45,15 @@ struct ImprovedTerseObjectUpdate32 {
 //  
 //  The 48-byte update. Adds a foot plane normal
 //
-struct ImprovedTerseObjectUpdate48 {
+struct ObjectUpdateData48 {
     LLVector4Net footplane;                 // foot plane
-    ImprovedTerseObjectUpdate32 update32;   // contains a 32-byte update
+    ObjectUpdateData32 update32;   // contains a 32-byte update
 };
 
 //
 //  The 60-byte update.  32 bit values
 //
-struct ImprovedTerseObjectUpdate60 {
+struct ObjectUpdateData60 {
     LLVector3Net position;                  // higher resolution values
     LLVector3Net velocity;
     LLVector3Net acceleration;
@@ -60,28 +62,28 @@ struct ImprovedTerseObjectUpdate60 {
 };
 
 //
-//  The 76-byte update.  Adds a foot plane  normal
+//  The 76-byte update.  Adds a foot plane normal
 //
-struct ImprovedTerseObjectUpdate76 {
+struct ObjectUpdateData76 {
     LLVector4Net footplane;
-    ImprovedTerseObjectUpdate60 update60;   // contains a 60-byte update
+    ObjectUpdateData60 update60;   // contains a 60-byte update
 };
 
 //
-//  All the alternatives for ImprovedTerseObjectUpdate
+//  All the alternatives for ObjectUpdateData
 //
-union ImprovedTerseObjectUpdate {
-    ImprovedTerseObjectUpdate16 update16;
-    ImprovedTerseObjectUpdate32 update32;
-    ImprovedTerseObjectUpdate48 update48;
-    ImprovedTerseObjectUpdate60 update60;
-    ImprovedTerseObjectUpdate76 update76;
+union ObjectUpdateData {
+    ObjectUpdateData16 update16;
+    ObjectUpdateData32 update32;
+    ObjectUpdateData48 update48;
+    ObjectUpdateData60 update60;
+    ObjectUpdateData76 update76;
     //  Check all sizes at compile time.
-    static_assert(sizeof(ImprovedTerseObjectUpdate16) == 16, "ImprovedTerseObjectUpdate16 struct is wrong size");   // must be exactly this size
-    static_assert(sizeof(ImprovedTerseObjectUpdate32) == 32, "ImprovedTerseObjectUpdate32 struct is wrong size");   // must be exactly this size
-    static_assert(sizeof(ImprovedTerseObjectUpdate48) == 48, "ImprovedTerseObjectUpdate48 struct is wrong size");   // must be exactly this size
-    static_assert(sizeof(ImprovedTerseObjectUpdate60) == 60, "ImprovedTerseObjectUpdate60 struct is wrong size");   // must be exactly this size    
-    static_assert(sizeof(ImprovedTerseObjectUpdate76) == 76, "ImprovedTerseObjectUpdate60 struct is wrong size");   // must be exactly this size
+    static_assert(sizeof(ObjectUpdateData16) == 16, "ObjectUpdateData16 struct is wrong size");   // must be exactly this size
+    static_assert(sizeof(ObjectUpdateData32) == 32, "ObjectUpdateData32 struct is wrong size");   // must be exactly this size
+    static_assert(sizeof(ObjectUpdateData48) == 48, "ObjectUpdateData48 struct is wrong size");   // must be exactly this size
+    static_assert(sizeof(ObjectUpdateData60) == 60, "ObjectUpdateData60 struct is wrong size");   // must be exactly this size    
+    static_assert(sizeof(ObjectUpdateData76) == 76, "ObjectUpdateData60 struct is wrong size");   // must be exactly this size
 
 
 };
