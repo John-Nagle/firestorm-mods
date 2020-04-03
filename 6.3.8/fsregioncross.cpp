62142305cc998a2b315a20bf0d7295cb26d840e3
John-Nagle <nagle@sitetruth.com>
2020-04-03 11:00:49 -0700

    Clean up region cross module. Use defaults that worked in test

--- 6.3.8/fsregioncross.cpp
+++ 6.3.8/fsregioncross.cpp
@@ -40,16 +40,28 @@
 #endif // UNITTEST
 
 //
-//  Improved region crossing prediction.
+//  Improved region crossing time limit prediction.
 //
-//  Debug tuning parameters. This code will try to limit the maximum position and angle error to the specified limits.
-//  The limits can be adjusted as debug symbols, but that should not be necessary.
+//  Applied when 
+//  1) object has an avatar sitting on it
+//  2) object has been seen to move since sat on
+//  3) object is outside the 0..256m region boundaries.
+//
+//  In other words, a vehicle with an avatar doing a region crossing.
+//
+//  This computes a safe time limit for extrapolating vehicle position and 
+//  rotation during a region crossing, when the simulator to simulator handoff
+//  is in progress and no object updates are being received. Goal is to keep
+//  the position and rotation errors within set bounds.
+//
+//  The smoother the movement of the vehicle approaching the sim crossing,
+//  the longer the allowed extrapolation will be.
 //
 //
 //  Constructor -- called when something sits on the object.
 //
 //  Potential vehicle, but might be just a chair.
-//  We don't start the movement averaging until there's movement.
+//  We don't start the movement averaging until there's movement at least once.
 //
 RegionCrossExtrapolateImpl::RegionCrossExtrapolateImpl(const LLViewerObject& vo) :          // constructor
     mOwner(vo),                                                     // back ref to owner
@@ -80,8 +92,8 @@ void RegionCrossExtrapolateImpl::update()
     F64 now = LLFrameTimer::getElapsedSeconds();                // timestamp
     if (mPreviousUpdateTime != 0.0)
     {   dt = now - mPreviousUpdateTime;                         // change since last update
-        //  ***SHOULD ADJUST FOR PING TIME AND TIME DILATION***
-        //  ***IS THERE SOME BETTER WAY TO GET THE TIME OF AN OBJECT UPDATE?***
+        //  Could adjust here for ping time and time dilation, but the filter isn't that
+        //  sensitive to minor variations dt and it would just complicate things.
     }
     mPreviousUpdateTime = now;
     LLQuaternion rot = mOwner.getRotationRegion();              // transform in global coords
@@ -89,12 +101,12 @@ void RegionCrossExtrapolateImpl::update()
     LLVector3 vel = mOwner.getVelocity()*inverserot;            // velocity in object coords
     LLVector3 angvel = mOwner.getAngularVelocity()*inverserot;  // angular velocity in object coords
     mFilteredVel.update(vel,dt);                                // accum into filter in object coords
-    mFilteredAngVel.update(angvel,dt);                          // accum into filter 
+    mFilteredAngVel.update(angvel,dt);                          // accum into filter in object coords
 #ifdef UNITTEST
     printf("dt: %6.3f     vel: <%4.3f,%4.3f,%4.3f>     filteredvel: <%4.3f,%4.3f,%4.3f>\n", dt,vel.mV[VX],vel.mV[VY],vel.mV[VZ], 
-        mFilteredVel.get().mV[VX], mFilteredVel.get().mV[VY],mFilteredVel.get().mV[VZ]); // ***TEMP***
+        mFilteredVel.get().mV[VX], mFilteredVel.get().mV[VY],mFilteredVel.get().mV[VZ]); 
     printf("dt: %6.3f  angvel: <%4.3f,%4.3f,%4.3f>  filteredangvel: <%4.3f,%4.3f,%4.3f>\n", dt,angvel.mV[VX],angvel.mV[VY],angvel.mV[VZ], 
-        mFilteredAngVel.get().mV[VX], mFilteredAngVel.get().mV[VY],mFilteredAngVel.get().mV[VZ]); // ***TEMP***
+        mFilteredAngVel.get().mV[VX], mFilteredAngVel.get().mV[VY],mFilteredAngVel.get().mV[VZ]); 
 #endif // UNITTEST
 }
 //
@@ -115,17 +127,19 @@ static inline F32 dividesafe(F32 num, F32 denom)
 //
 F32 RegionCrossExtrapolateImpl::getextraptimelimit() const
 {
+    //  Debug tuning parameters. This code will try to limit the maximum position and angle error to the specified limits.
+    //  The limits can be adjusted as debug symbols or in settings.xml, but that should not be necessary.
     static LLCachedControl<F32> fsRegionCrossingPositionErrorLimit(gSavedSettings, "FSRegionCrossingPositionErrorLimit");
     static LLCachedControl<F32> fsRegionCrossingAngleErrorLimit(gSavedSettings, "FSRegionCrossingAngleErrorLimit");   
 #ifdef UNITTEST                                                                 // Unit test only, set in XML in Firestorm.
-    fsRegionCrossingPositionErrorLimit.mValue = 1.0;                            // (m) default position error limit
+    fsRegionCrossingPositionErrorLimit.mValue = 0.25;                           // (m) default position error limit
     fsRegionCrossingAngleErrorLimit.mValue = 20.0;                              // (degrees) default angle error limit
 #endif // UNITTEST
    //  Time limit is max allowed error / error. Returns worst case (smallest) of vel and angular vel limits.
     LLQuaternion rot = mOwner.getRotationRegion();              // transform in global coords
     const LLQuaternion& inverserot = rot.conjugate();           // transform global to local
-#ifdef UNITTEST
-    printf("velerr: %4.3f  allowed: %4.3f   angvelerr: %4.3f  allowed: %4.3f\n",               // ***TEMP***
+#ifdef UNITTEST                                                 // only used in offline testing
+    printf("velerr: %4.3f  allowed: %4.3f   angvelerr: %4.3f  allowed: %4.3f\n",               
         (mOwner.getVelocity()*inverserot - mFilteredVel.get()).length(),
         F32(fsRegionCrossingPositionErrorLimit),
         (mOwner.getAngularVelocity()*inverserot - mFilteredAngVel.get()).length()*(180/M_PI),
@@ -165,14 +179,24 @@ BOOL RegionCrossExtrapolate::ifsaton(const LLViewerObject& vo)  // true if root 
 //
 //  Works on vectors.
 //
-//  Filter function is scaled by 1- 1/((1+(1/filterval))^t)
+//  Filter function is scaled by 1- 1/((1+(1/filterval))^secs)
 //  This is so samples which represent more time are weighted more.
+//
+//  The filterval function is a curve which goes through (0,0) and gradually
+//  approaches 1 for larger values of secs.  Secs is the time since the last update.
+//  We assume that the time covered by an update had reasonably uniform velocity
+//  and angular velocity, since if those were changing rapidly, the server would send
+//  more object updates.
+//
+//  Setting filter smoothing time to zero turns off the filter and results in the predictor
+//  always returning a very large value and not affecting region crossing times.
+//  So that's how to turn this off, if desired.
 //
 void LowPassFilter::update(const LLVector3& val, F32 secs)      // add new value into filter
 {
     static LLCachedControl<F32> fsRegionCrossingSmoothingTime(gSavedSettings, "FSRegionCrossingSmoothingTime");
 #ifdef UNITTEST                                                 // Unit test only, off in Firestorm
-    fsRegionCrossingSmoothingTime.mValue = 2.0;                 // default smoothing time for predictor 
+    fsRegionCrossingSmoothingTime.mValue = 10.0;                // default smoothing time for predictor 
 #endif // UNITTEST
     if (!mInitialized)                                          // if not initialized yet
     {   mFiltered = val;                                        // just use new value
